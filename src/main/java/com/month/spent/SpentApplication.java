9d25e2c76c7efd1025a8211f9bfc4359044ed4bc
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-03-03 09:18:13 +0000

    init projet

---
+++ src/main/java/com/month/spent/SpentApplication.java
@@ -0,0 +1,13 @@
+package com.month.spent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/month/spent/SpentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.month.spent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
