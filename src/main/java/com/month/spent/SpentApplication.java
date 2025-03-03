709f12dbe0d7692a728c714336a146580c79dec3
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-03-03 10:37:33 +0000

    Implement super admin creation on application startup and add properties for super admin credentials

--- src/main/java/com/month/spent/SpentApplication.java
+++ src/main/java/com/month/spent/SpentApplication.java
@@ -1,13 +1,36 @@
 package com.month.spent;
 
+import com.month.spent.service.UtilisateurService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpentApplication {
+public class SpentApplication implements CommandLineRunner {
+	@Value("${super.admin.email}")
+	private String EMAIL;
+	@Value("${super.admin.password}")
+	private String PASSWORD;
+	@Value("${super.admin.username}")
+	private String USERNAME;
+
+	private final UtilisateurService utilisateurService;
+
+	public SpentApplication(UtilisateurService utilisateurService) {
+		this.utilisateurService = utilisateurService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpentApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			utilisateurService.createSuprerAdmin(EMAIL, USERNAME, PASSWORD);
+		} catch (Exception e) {
+			System.out.println();
+		}
+	}
 }
